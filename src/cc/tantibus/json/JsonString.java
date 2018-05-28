5fe2cbc236ebb067b614e5bceda31df3a0d9b5b4
0da <35634902+0da@users.noreply.github.com>
2018-05-29 00:56:41 +0500

    JsonString updated.

--- src/cc/tantibus/json/JsonString.java
+++ src/cc/tantibus/json/JsonString.java
@@ -1,5 +1,8 @@
 package cc.tantibus.json;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 /**
  * 0da - 30.01.2018.
  */
@@ -11,7 +14,81 @@ public class JsonString implements JsonStruct {
     }
 
     @Override
+    public Byte asByte() {
+        try {
+            return Byte.parseByte(value, 10);
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+    @Override
+    public Short asShort() {
+        try {
+            return Short.parseShort(value, 10);
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+    @Override
+    public Integer asInt() {
+        try {
+            return Integer.parseInt(value, 10);
+        } catch (Exception e) {
+            return 0;
+        }
+    }
+
+    @Override
+    public Long asLong() {
+        try {
+            return Long.parseLong(value, 10);
+        } catch (Exception e) {
+            return 0L;
+        }
+    }
+
+    @Override
+    public Float asFloat() {
+        try {
+            return Float.parseFloat(value);
+        } catch (Exception e) {
+            return 0f;
+        }
+    }
+
+    @Override
+    public Double asDouble() {
+        try {
+            return Double.parseDouble(value);
+        } catch (Exception e) {
+            return 0d;
+        }
+    }
+
+    @Override
+    public BigInteger asBigInteger() {
+        try {
+            return new BigInteger(value, 10);
+        } catch (Exception e) {
+            return BigInteger.ZERO;
+        }
+    }
+
+    @Override
+    public BigDecimal asBigDecimal() {
+        try {
+            return new BigDecimal(value);
+        } catch (Exception e) {
+            return BigDecimal.ZERO;
+        }
+    }
+
+    @Override
     public String toString() {
         return value;
     }
+
+
 }
